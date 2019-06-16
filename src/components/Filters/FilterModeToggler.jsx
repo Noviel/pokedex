5ab09b5a761ca442d06b4a743679705072a6977c
Alexander Snov <snov.contact@gmail.com>
2019-06-17 01:16:23 +0700

    Prettify search mode a bit

--- src/components/Filters/FilterModeToggler.jsx
+++ src/components/Filters/FilterModeToggler.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Observer } from 'mobx-react-lite';
 
+import { makeStyles } from '@material-ui/core/styles';
 import Tooltip from '@material-ui/core/Tooltip';
 import Typography from '@material-ui/core/Typography';
 import InfoIcon from '@material-ui/icons/Info';
@@ -9,23 +10,31 @@ import { Switch } from '../ui/Switch';
 
 import { useStore } from '../StoreContext';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: 'flex'
+  },
+}));
+
 const FilterModeToggler = () => {
+  const classes = useStyles();
   const store = useStore();
+
   return (
     <Observer>
       {() => (
-        <>
-          <Typography>
-            Global search{' '}
-            <Tooltip title="Search the entire database. It takes some time to preload indices.">
-              <InfoIcon />
-            </Tooltip>
-          </Typography>
+        <div className={classes.root}>
           <Switch
             checked={store.isGlobalSearch}
             onChange={store.toggleSearchMode}
           />
-        </>
+          <Typography>
+            Global search
+            <Tooltip title="Search the entire database. It takes some time to preload indices.">
+              <InfoIcon />
+            </Tooltip>
+          </Typography>
+        </div>
       )}
     </Observer>
   );
