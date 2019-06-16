e444f3bb1d892ec714770da30e995fcb25c2f8b3
Alexander Snov <snov.contact@gmail.com>
2019-06-17 01:49:58 +0700

    Fix pokemon card being overlapped by bottom bar

--- src/components/PokemonsList.jsx
+++ src/components/PokemonsList.jsx
@@ -2,30 +2,35 @@ import React from 'react';
 import { Observer } from 'mobx-react-lite';
 
 import Grid from '@material-ui/core/Grid';
+import { makeStyles } from '@material-ui/core/styles';
 
 import { useStore } from './StoreContext';
 
 import { PokemonListItem } from './PokemonListItem';
 
+const useStyles = makeStyles(theme => ({
+  root: {
+    marginBottom: theme.spacing(10),
+  },
+}));
+
 const PokemonsList = () => {
   const store = useStore();
+  const classes = useStyles();
   return (
     <Observer>
       {() => (
-        <div>
-          {store.searchStatus}
-          <Grid container spacing={2}>
-            {store.visiblePokemons.length ? (
-              store.visiblePokemons.map(name => (
-                <Grid key={name} item xs={12} sm={6} md={4}>
-                  <PokemonListItem name={name} />
-                </Grid>
-              ))
-            ) : (
-              <div>NotFound</div>
-            )}
-          </Grid>
-        </div>
+        <Grid container spacing={2} className={classes.root}>
+          {store.visiblePokemons.length ? (
+            store.visiblePokemons.map(name => (
+              <Grid key={name} item xs={12} sm={6} md={4}>
+                <PokemonListItem name={name} />
+              </Grid>
+            ))
+          ) : (
+            <div>NotFound</div>
+          )}
+        </Grid>
       )}
     </Observer>
   );
