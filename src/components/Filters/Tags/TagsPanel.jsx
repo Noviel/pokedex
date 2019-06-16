77e4533d7317e1236a815d140cd2b24423ff2b18
Alexander Snov <snov.contact@gmail.com>
2019-06-17 02:18:24 +0700

    Add margin to tags

--- src/components/Filters/Tags/TagsPanel.jsx
+++ src/components/Filters/Tags/TagsPanel.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 
+import { makeStyles } from '@material-ui/core/styles';
+
 import { TagsList } from './TagsList';
 import { AddTag } from './AddTag';
 
+const useStyles = makeStyles({
+  divider: {
+    marginBottom: 10,
+  },
+});
+
 const TagsPanel = () => {
+  const classes = useStyles();
+
   return (
     <>
-      <TagsList />
       <AddTag />
+      <div className={classes.divider} />
+      <TagsList />
     </>
   );
 };
